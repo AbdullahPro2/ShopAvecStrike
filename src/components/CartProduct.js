3f9a92c4d2629dd733ec14d92a661602eb70f53c
AbdullahPro2 <zeb16208@gmail.com>
2023-12-22 22:06:12 +0100

    Deleting Item from Cart Functionlity

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "../styles/product.css";
-
+import dustbin from "../Assets/dustbin.png";
 function CartProduct({ item, quantity }) {
+  function handleDustbin() {}
   return (
     <div className="product">
       <img src={item.image} alt="item iocn" className="product-img" />
       <div className="product-detail">
         <h4>{item.name}</h4>
         <p>{item.new_price} €</p>
-        <p>Quantity: {quantity}</p>
+        <div className="dustbin">
+          <p>Quantity: {quantity}</p>
+          <img src={dustbin} alt="dust bin" onClick={handleDustbin} />
+        </div>
       </div>
       <button className="add-cart category-prodcut-button">Buy Now</button>
     </div>

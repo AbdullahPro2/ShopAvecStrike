c2490068db5cb5cae789901d7be165bd09c93de9
AbdullahPro2 <zeb16208@gmail.com>
2023-12-25 18:37:44 +0100

    lazy loading images

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import CartBtn from "./CartBtn";
+import LazyLoad from "react-lazyload";
 
 function Product({ item, setShowAlert }) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <div className="product">
-      <img src={item.image} alt="item iocn" className="product-img" />
+      {/* Conditionally render the loader */}
+      {!imageLoaded && <div className="loader"></div>}
+
+      {/* LazyLoad component for the image */}
+      <LazyLoad height={200} offset={100}>
+        {/* Actual image with onLoad event handler */}
+        <img
+          src={item.image}
+          alt="item icon"
+          className={`product-img ${imageLoaded ? "loaded" : ""}`}
+          onLoad={handleImageLoad}
+        />
+      </LazyLoad>
+
       <div className="product-detail">
         <h4>{item.name}</h4>
         <p>{item.new_price} €</p>
